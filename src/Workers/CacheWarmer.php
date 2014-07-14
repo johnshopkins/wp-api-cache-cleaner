a628635c62851200e4bcc097e21904fee22b2e91
Jen Wachter <jenleighkelly@gmail.com>
2014-07-14 12:04:50 -0400

    Uncommented all the content types.

--- src/Workers/CacheWarmer.php
+++ src/Workers/CacheWarmer.php
@@ -17,24 +17,24 @@ class CacheWarmer extends BaseWorker
   protected $contentTypes = array(
 
     // no embeds
-    // "attachment",
-    // "block",
-    // "club",
+    "attachment",
+    "block",
+    "club",
     "division",
-    // "related_content",
+    "related_content",
 
-    // // embeds
-    // "fact",             // attachment
-    // "field_of_study",   // division
-    // "instagram_media",  // location
-    // "location",         // attachment
-    // "timeline_event",   // attachment
+    // embeds
+    "fact",             // attachment
+    "field_of_study",   // division
+    "instagram_media",  // location
+    "location",         // attachment
+    "timeline_event",   // attachment
 
-    // "map",              // attachment, location
-    // "page",             // block, attachment
+    "map",              // attachment, location
+    "page",             // block, attachment
     
-    // "person",           // attachment, field_of_study, club, division
-    // "quote"            // person
+    "person",           // attachment, field_of_study, club, division
+    "quote"            // person
     
   );
 
