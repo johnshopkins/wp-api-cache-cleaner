d62205ca42350635529af68f8d4059cc13b31765
Jen Wachter <jenleighkelly@gmail.com>
2014-10-06 14:08:56 -0400

    Added data logging for workloads that contain endpoints.

--- src/Workers/CacheCleaner.php
+++ src/Workers/CacheCleaner.php
@@ -36,10 +36,10 @@ class CacheCleaner extends BaseWorker
   }
 
   public function clean(\GearmanJob $job)
-{
+  {
     $workload = json_decode($job->workload());
 
-    if ($workload) {
+    if (isset($workload->post)) {
 
       echo $this->getDate() . " Starting API cache clearing for post #{$workload->post->ID}.\n";
 
@@ -49,6 +49,17 @@ class CacheCleaner extends BaseWorker
         echo $this->getDate() . " API cache cleared for post #{$workload->post->ID}.\n";
       } else {
         echo $this->getDate() . " API cache did not need to be cleared for post #{$workload->post->ID} (revision).\n";
+      }
+    }
+
+    if (isset($workload->endpoint)) {
+
+      echo $this->getDate() . " Starting API cache clearing for endpoint /{$workload->endpoint}.\n";
+
+      $result = $this->clearCache($workload);
+      
+      if ($result) {
+        echo $this->getDate() . " API cache cleared for endpoint /{$workload->endpoint}.\n";
       }
 
     }
