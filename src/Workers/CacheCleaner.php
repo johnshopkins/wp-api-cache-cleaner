5a20ab85e257075be2767c35b133e8e99282a0f6
Jen Wachter <jenleighkelly@gmail.com>
2016-02-25 12:47:54 -0500

    Added some troubleshooting logs

--- src/Workers/CacheCleaner.php
+++ src/Workers/CacheCleaner.php
@@ -46,6 +46,11 @@ class CacheCleaner
 
     $this->http->get($url, array("clear_cache" => true));
 
+    $response = $this->http->response;
+    unset($response["body"]);
+    unset($response["body_raw"]);
+    print_r($response);
+
     $status = $this->http->getStatusCode();
     if ($status != 200) {
       $this->logger->addWarning("Endpoint `{$endpoint}` was unable to be cleared. Returned with a status of {$status}", $this->http->getBody());
